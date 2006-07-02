f9c070257a5b72a7032221ecb501f92211c4a7d3
Gunnar Hillert <gunnar@hillert.com>
2006-07-02 02:53:38 +0000

    Updated JS libraries and added rico for rounded corners.

--- src/main/webapp/js/ajaxtags.js
+++ src/main/webapp/js/ajaxtags.js
@@ -1,12 +1,12 @@
 /**
  * Copyright 2005 Darren L. Spurgeon
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -897,7 +897,7 @@ AjaxJspTag.Portlet.prototype = Object.extend(new AjaxJspTag.Base(), {
 Ajax.XmlToHtmlAutocompleter = Class.create();
 Object.extend(Object.extend(Ajax.XmlToHtmlAutocompleter.prototype, Autocompleter.Base.prototype), {
   initialize: function(element, update, url, options) {
-    this.baseInitialize(element, update, options);
+	  this.baseInitialize(element, update, options);
     this.options.asynchronous  = true;
     this.options.onComplete    = this.onComplete.bind(this);
     this.options.defaultParams = this.options.parameters || null;
@@ -905,7 +905,7 @@ Object.extend(Object.extend(Ajax.XmlToHtmlAutocompleter.prototype, Autocompleter
   },
 
   getUpdatedChoices: function() {
-    entry = encodeURIComponent(this.options.paramName) + '=' +
+    entry = encodeURIComponent(this.options.paramName) + '=' + 
       encodeURIComponent(this.getToken());
 
     this.options.parameters = this.options.callback ?
