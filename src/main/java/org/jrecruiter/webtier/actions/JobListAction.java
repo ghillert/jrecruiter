bd6248bb675cd45f9965511a4e2a981c99d164b0
Gunnar Hillert <gunnar@hillert.com>
2006-10-16 03:45:45 +0000

    * correcting some code imports

--- src/main/java/org/jrecruiter/webtier/actions/JobListAction.java
+++ src/main/java/org/jrecruiter/webtier/actions/JobListAction.java
@@ -16,22 +16,21 @@
 package org.jrecruiter.webtier.actions;
 
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.log4j.Logger;
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.displaytag.properties.SortOrderEnum;
-import org.jfree.util.SortOrder;
-import org.jrecruiter.dao.TestJobsDAO;
 import org.jrecruiter.service.JobServiceInterface;
 import org.jrecruiter.webtier.JobsForDisplaytagSorting;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 /**
  *
