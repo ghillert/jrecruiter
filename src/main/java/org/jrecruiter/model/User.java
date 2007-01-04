6031b8a83b7a19589fc28df67007d4a42d273b2b
ghillert <ghillert@476599a2-9835-11de-a05d-3bd73e269465>
2007-01-04 03:13:25 +0000

    Minor fixes

--- src/main/java/org/jrecruiter/model/User.java
+++ src/main/java/org/jrecruiter/model/User.java
@@ -20,17 +20,35 @@ import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
 import org.acegisecurity.GrantedAuthority;
 import org.acegisecurity.userdetails.UserDetails;
-import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.hibernate.validator.Email;
 
 /**
  * This class represents a user.
  *
- * @author Dorota Puchala, Gunnar Hillert
+ * @author Gunnar Hillert
  * @version @version $Id$
  */
+@Entity
+@Table(name="users"
+    ,schema="public"
+    , uniqueConstraints = {  }
+)
 public class User extends BaseObject implements Serializable, UserDetails{
 
     /**
@@ -38,25 +56,25 @@ public class User extends BaseObject implements Serializable, UserDetails{
      */
     private static final long serialVersionUID = -8704683609547598214L;
 
-    private String firstName;
-    private String lastName;
-    private String company;
-    private String phone;
-    private String fax;
-    private String email;
-    private Date registerDate;
-    private Date updateDate;
-    private Set < UserRole > roles;
-    private List userJobs;
-
-    //~~ Acegi
-    private GrantedAuthority[] authorities;
-    private String password;
-    private String username;
     private boolean accountExpired;
     private boolean accountLocked;
+    //~~ Acegi
+    private GrantedAuthority[] authorities;
+    private String company;
     private boolean credentialsExpired;
+    private String email;
     private boolean enabled = true;
+    private String fax;
+    private String firstName;
+    private String lastName;
+
+    private String password;
+    private String phone;
+    private Date registerDate;
+    private Set < UserRole > roles;
+    private Date updateDate;
+    private List < Job > jobs;
+    private String username;
 
 
     /**
@@ -79,286 +97,6 @@ public class User extends BaseObject implements Serializable, UserDetails{
     }
 
     /**
-     *
-     * @return
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     *
-     * @param firstName
-     */
-    public void setFirstName(final String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     *
-     * @return
-     */
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     *
-     * @param lastName
-     */
-    public void setLastName(final String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * @return Returns the company.
-     */
-    public String getCompany() {
-        return company;
-    }
-    /**
-     * @param company The company to set.
-     */
-    public void setCompany(final String company) {
-        this.company = company;
-    }
-
-    /**
-     *
-     * @return
-     */
-    public String getPhone() {
-        return phone;
-    }
-
-    /**
-     *
-     * @param phone
-     */
-    public void setPhone(final String phone) {
-        this.phone = phone;
-    }
-
-    /**
-     *
-     * @return
-     */
-    public String getFax() {
-        return fax;
-    }
-
-    /**
-     *
-     * @param fax
-     */
-    public void setFax(final String fax) {
-        this.fax = fax;
-    }
-
-    /**
-     *
-     * @return
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     *
-     * @param email
-     */
-    public void setEmail(final String email) {
-        this.email = email;
-    }
-
-    /**
-     *
-     * @return
-     */
-    public Date getRegisterDate() {
-        return registerDate;
-    }
-
-    /**
-     *
-     * @param registerDate
-     */
-    public void setRegisterDate(Date registerDate) {
-        this.registerDate = registerDate;
-    }
-
-    /**
-     *
-     * @return
-     */
-    public Date getUpdateDate() {
-        return updateDate;
-    }
-
-    /**
-     *
-     * @param updateDate
-     */
-    public void setUpdateDate(Date updateDate) {
-        this.updateDate = updateDate;
-    }
-
-    /**
-     *
-     * @return
-     */
-    public List getUserJobs() {
-        return userJobs;
-    }
-
-    /**
-     *
-     * @param userJobs
-     */
-    public void setUserJobs(List userJobs) {
-        this.userJobs = userJobs;
-    }
-
-
-    /**
-     * @return Returns the roles.
-     */
-    public Set<UserRole> getRoles() {
-        return roles;
-    }
-
-    /**
-     * @param roles The roles to set.
-     */
-    public void setRoles(Set<UserRole> roles) {
-        this.roles = roles;
-    }
-
-    //~~~~~~~~ Implemented from Acegi Seurity ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    /**
-     * @see org.acegisecurity.userdetails.UserDetails#isAccountNonExpired()
-     * @return Returns the accountNonExpired.
-     */
-    public boolean isAccountNonExpired() {
-        return !accountExpired;
-    }
-
-    /**
-     * @see org.acegisecurity.userdetails.UserDetails#isAccountNonLocked()
-     * @return Returns the accountNonLocked.
-     */
-    public boolean isAccountNonLocked() {
-        return !accountLocked;
-    }
-
-    /**
-     * @see org.acegisecurity.userdetails.UserDetails#getAuthorities()
-     * @return Returns the authorities.
-     */
-    public GrantedAuthority[] getAuthorities() {
-        if (roles != null) {
-            return (GrantedAuthority[]) roles.toArray(new GrantedAuthority[0]);
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * @see org.acegisecurity.userdetails.UserDetails#isCredentialsNonExpired()
-     * @return Returns the credentialsNonExpired.
-     */
-    public boolean isCredentialsNonExpired() {
-        return !credentialsExpired;
-    }
-
-    /**
-     * @see org.acegisecurity.userdetails.UserDetails#isEnabled()
-     * @return Returns the enabled.
-     */
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    /**
-     * @param enabled The enabled to set.
-     */
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    /**
-     * @see org.acegisecurity.userdetails.UserDetails#getPassword()
-     * @return Returns the password.
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * @param password The password to set.
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    /**
-     * @see org.acegisecurity.userdetails.UserDetails#getUsername()
-     * @return Returns the username.
-     */
-    public String getUsername() {
-        return username;
-    }
-
-    /**
-     * @param username The username to set.
-     */
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    /**
-     * @return Returns the accountExpired.
-     */
-    public boolean isAccountExpired() {
-        return accountExpired;
-    }
-
-    /**
-     * @param accountExpired The accountExpired to set.
-     */
-    public void setAccountExpired(boolean accountExpired) {
-        this.accountExpired = accountExpired;
-    }
-
-    /**
-     * @return Returns the accountLocked.
-     */
-    public boolean isAccountLocked() {
-        return accountLocked;
-    }
-
-    /**
-     * @param accountLocked The accountLocked to set.
-     */
-    public void setAccountLocked(boolean accountLocked) {
-        this.accountLocked = accountLocked;
-    }
-
-    /**
-     * @return Returns the credentialsExpired.
-     */
-    public boolean isCredentialsExpired() {
-        return credentialsExpired;
-    }
-
-    /**
-     * @param credentialsExpired The credentialsExpired to set.
-     */
-    public void setCredentialsExpired(boolean credentialsExpired) {
-        this.credentialsExpired = credentialsExpired;
-    }
-
-    /**
      * @see java.lang.Object#equals(java.lang.Object)
      */
     @Override
@@ -377,6 +115,124 @@ public class User extends BaseObject implements Serializable, UserDetails{
     }
 
     /**
+     * @see org.acegisecurity.userdetails.UserDetails#getAuthorities()
+     * @return Returns the authorities.
+     */
+    @Transient
+    public GrantedAuthority[] getAuthorities() {
+        if (roles != null) {
+            return (GrantedAuthority[]) roles.toArray(new GrantedAuthority[0]);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @return Returns the company.
+     */
+    @Column(name="company", unique=false, nullable=true, insertable=true, updatable=true, length=25)
+    public String getCompany() {
+        return company;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Column(name="email", unique=false, nullable=true, insertable=true, updatable=true, length=50)
+    @Email
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Column(name="fax", unique=false, nullable=true, insertable=true, updatable=true, length=25)
+    public String getFax() {
+        return fax;
+    }
+    /**
+     *
+     * @return
+     */
+    @Column(name="first_name", unique=false, nullable=true, insertable=true, updatable=true, length=50)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Column(name="last_name", unique=false, nullable=true, insertable=true, updatable=true, length=50)
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @see org.acegisecurity.userdetails.UserDetails#getPassword()
+     * @return Returns the password.
+     */
+    @Column(name="user_passwd", unique=false, nullable=false, insertable=true, updatable=true, length=25)
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Column(name="phone", unique=false, nullable=true, insertable=true, updatable=true, length=25)
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Column(name="register_date", unique=false, nullable=true, insertable=true, updatable=true, length=8)
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    @ManyToMany(cascade = { CascadeType.ALL }, fetch = FetchType.LAZY, mappedBy = "users")
+    public Set<UserRole> getRoles() {
+        return roles;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Column(name="update_date", unique=false, nullable=true, insertable=true, updatable=true, length=8)
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    /**
+     *
+     * @return
+     */
+  //  @OneToMany(mappedBy="job")
+    @Transient
+    public List < Job >getUserJobs() {
+        return jobs;
+    }
+
+    /**
+     * @see org.acegisecurity.userdetails.UserDetails#getUsername()
+     * @return Returns the username.
+     */
+    @Id
+    @Column(name="user_name", unique=true, nullable=false, insertable=true, updatable=true, length=25)
+    public String getUsername() {
+        return username;
+    }
+
+    /**
      * @see java.lang.Object#hashCode()
      */
     @Override
@@ -385,6 +241,188 @@ public class User extends BaseObject implements Serializable, UserDetails{
                 .append(lastName).append(company).append(phone).append(fax)
                 .append(email).append(registerDate).append(updateDate).append(
                         password).append(username).toHashCode();
+    }
+
+    /**
+     * @return Returns the accountExpired.
+     */
+    @Transient
+    public boolean isAccountExpired() {
+        return accountExpired;
+    }
+
+    /**
+     * @return Returns the accountLocked.
+     */
+    @Transient
+    public boolean isAccountLocked() {
+        return accountLocked;
+    }
+
+    //~~~~~~~~ Implemented from Acegi Seurity ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    /**
+     * @see org.acegisecurity.userdetails.UserDetails#isAccountNonExpired()
+     * @return Returns the accountNonExpired.
+     */
+    @Transient
+    public boolean isAccountNonExpired() {
+        return !accountExpired;
+    }
+
+
+    /**
+     * @see org.acegisecurity.userdetails.UserDetails#isAccountNonLocked()
+     * @return Returns the accountNonLocked.
+     */
+    @Transient
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+
+    /**
+     * @return Returns the credentialsExpired.
+     */
+    @Transient
+    public boolean isCredentialsExpired() {
+        return credentialsExpired;
+    }
+
+    /**
+     * @see org.acegisecurity.userdetails.UserDetails#isCredentialsNonExpired()
+     * @return Returns the credentialsNonExpired.
+     */
+    @Transient
+    public boolean isCredentialsNonExpired() {
+        return !credentialsExpired;
+    }
+
+    /**
+     * @see org.acegisecurity.userdetails.UserDetails#isEnabled()
+     * @return Returns the enabled.
+     */
+    @Transient
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    /**
+     * @param accountExpired The accountExpired to set.
+     */
+    public void setAccountExpired(boolean accountExpired) {
+        this.accountExpired = accountExpired;
+    }
+
+    /**
+     * @param accountLocked The accountLocked to set.
+     */
+    public void setAccountLocked(boolean accountLocked) {
+        this.accountLocked = accountLocked;
+    }
+
+    /**
+     * @param company The company to set.
+     */
+    public void setCompany(final String company) {
+        this.company = company;
+    }
+
+    /**
+     * @param credentialsExpired The credentialsExpired to set.
+     */
+    public void setCredentialsExpired(boolean credentialsExpired) {
+        this.credentialsExpired = credentialsExpired;
+    }
+
+    /**
+     *
+     * @param email
+     */
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    /**
+     * @param enabled The enabled to set.
+     */
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    /**
+     *
+     * @param fax
+     */
+    public void setFax(final String fax) {
+        this.fax = fax;
+    }
+
+    /**
+     *
+     * @param firstName
+     */
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     *
+     * @param lastName
+     */
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @param password The password to set.
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     *
+     * @param phone
+     */
+    public void setPhone(final String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     *
+     * @param registerDate
+     */
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    /**
+     * @param roles The roles to set.
+     */
+    public void setRoles(Set<UserRole> roles) {
+        this.roles = roles;
+    }
+
+    /**
+     *
+     * @param updateDate
+     */
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    /**
+     *
+     * @param userJobs
+     */
+    public void setUserJobs(List < Job > userJobs) {
+        this.jobs = userJobs;
+    }
+
+    /**
+     * @param username The username to set.
+     */
+    public void setUsername(String username) {
+        this.username = username;
     }
 
 
