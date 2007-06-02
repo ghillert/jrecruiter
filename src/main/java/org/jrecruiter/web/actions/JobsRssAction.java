65e32401dff114ef9801552a8a1ad97b376cc4a2
Gunnar Hillert <gunnar@hillert.com>
2007-06-02 21:17:14 +0000

    *Implemented an RSS feed using Struts 2 and Rome, showing the latest 20 jobs *Added a box (On job listing page) containing links to leading social bookmarking sites

---
+++ src/main/java/org/jrecruiter/web/actions/JobsRssAction.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package org.jrecruiter.web.actions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jrecruiter.model.Job;
+import org.jrecruiter.service.JobService;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.sun.syndication.feed.synd.SyndContent;
+import com.sun.syndication.feed.synd.SyndContentImpl;
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndEntryImpl;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.feed.synd.SyndFeedImpl;
+
+/**
+ * @author hillert
+ *
+ */
+public class JobsRssAction extends ActionSupport {
+
+	private SyndFeed rssFeed = new SyndFeedImpl();
+	private JobService jobService;
+	
+	/**
+	 * @return the jobService
+	 */
+	public JobService getJobService() {
+		return jobService;
+	}
+
+	/**
+	 * @param jobService the jobService to set
+	 */
+	public void setJobService(JobService jobService) {
+		this.jobService = jobService;
+	}
+
+	/**
+	 * @return the rssFeed
+	 */
+	public SyndFeed getRssFeed() {
+		return rssFeed;
+	}
+
+
+
+	/**
+	 * @param rssFeed the rssFeed to set
+	 */
+	public void setRssFeed(SyndFeed rssFeed) {
+		this.rssFeed = rssFeed;
+	}
+
+
+
+	public String execute() {
+        rssFeed.setFeedType("rss_1.0");
+		rssFeed.setTitle("AJUG Job Postings");
+		rssFeed.setLink("http://www.ajug.org/jobs");
+		rssFeed.setDescription("RSS feed of the Atlanta Java User Group's job posting service.");
+
+        List <SyndEntry>entries = new ArrayList<SyndEntry>();
+        SyndEntry entry;
+        SyndContent description;
+
+        List <Job> jobs = jobService.getJobs(20, 1, "updateDate", "DESC");
+        
+        for (Job job : jobs) {
+            entry = new SyndEntryImpl();
+            entry.setTitle(job.getJobTitle());
+            //entry.setLink("http://wiki.java.net/bin/view/Javawsxml/Rome01");
+            entry.setPublishedDate(job.getUpdateDate());
+            description = new SyndContentImpl();
+            description.setType("text/plain");
+            description.setValue(job.getDescription());
+            entry.setDescription(description);
+            entries.add(entry);
+            entry.setLink("blubba");
+        }
+        
+        rssFeed.setEntries(entries);
+        
+		return SUCCESS;
+		
+	}
+
+}
--- src/main/webapp/WEB-INF/jsp/jobs.jsp
+++ src/main/webapp/WEB-INF/jsp/jobs.jsp
@@ -1,9 +1,23 @@
+<title>Job List</title>
 <%@ include file="/WEB-INF/jsp/includes/taglibs.jsp"%>
-<a href="<c:url value='/rss.jobs'/>">
-	<img src="<c:url value='/images/icons/feed-icon-28x28.png'/>" alt="Get the latest job postings as RSS feed" title="Get the latest job postings as RSS feed"/>    
-</a>    
+ 
 <%@include file="/WEB-INF/jsp/jobsTable.jsp"%>
 
 <div id="jobDetailContainer" style="display: none;"></div>
 
-
+<table id="socialinks">
+	<tr>
+		<td>
+			<a href='http://del.icio.us/post?url=http://www.ajug.org/jrecruiter/showJobs.html&title=AJUG+Job+Posting+Service' target='_blank'><img border='0' src='images/icons/delicious.png' alt='del.icio.us' title='del.icio.us' /></a>
+		</td>
+		<td>
+			<a href='http://www.stumbleupon.com/submit?url=http://www.ajug.org/jrecruiter/showJobs.html&amp;title=AJUG+Job+Posting+Service' target='_blank'><img border='0' src='images/icons/stumbleupon.png' alt='stumbleupon' title='stumbleupon' /></a>
+		</td>
+		<td>
+			<a href='http://www.google.com/bookmarks/mark?op=add&amp;hl=de&amp;bkmk=http://www.ajug.org/jrecruiter/showJobs.html&amp;annotation=&amp;labels=&amp;title=AJUG+Job+Posting+Service' target='_blank'><img border='0' src='images/icons/google.png' alt='google' title='google' /></a>
+		</td>
+		<td>
+			<a href='http://myweb2.search.yahoo.com/myresults/bookmarklet?u=http://www.ajug.org/jrecruiter/showJobs.html&t=AJUG+Job+Posting+Service' target='_blank'><img border='0' src='images/icons/yahoomyweb.png' alt='YahooMyWeb' title='YahooMyWeb' /></a>
+		</td>
+	</tr>
+</table>
