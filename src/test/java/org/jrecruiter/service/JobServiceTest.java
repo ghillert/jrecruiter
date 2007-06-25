1782514d148ebe4f0133562cc437218c2f74c42e
ghillert <ghillert@476599a2-9835-11de-a05d-3bd73e269465>
2007-06-25 02:14:47 +0000

    Added more tests

---
+++ src/test/java/org/jrecruiter/service/JobServiceTest.java
@@ -0,0 +1,65 @@
+/*
+*	http://www.jrecruiter.org
+*
+*	Disclaimer of Warranty.
+*
+*	Unless required by applicable law or agreed to in writing, Licensor provides
+*	the Work (and each Contributor provides its Contributions) on an "AS IS" BASIS,
+*	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied,
+*	including, without limitation, any warranties or conditions of TITLE,
+*	NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE. You are
+*	solely responsible for determining the appropriateness of using or
+*	redistributing the Work and assume any risks associated with Your exercise of
+*	permissions under this License.
+*
+*/
+package org.jrecruiter.service;
+
+import org.jrecruiter.test.BaseTest;
+
+/**
+ *
+ * @author Gunnar Hillert
+ * @version $Id: JobService.java 93 2007-01-22 02:42:14Z ghillert $
+ */
+public class JobServiceTest extends BaseTest {
+	
+	JobService jobService;
+	
+    /**
+	 * @param jobService the jobService to set
+	 */
+	public void setJobService(JobService jobService) {
+		this.jobService = jobService;
+	}
+
+	public void addJobTest(){}
+
+	public void deleteJobForIdTest(){}
+
+    public void getJobForIdTest(){}
+
+    public void getJobsTest(){}
+
+    public void getJobsTest2(){}
+
+    public void getJobsCountTest(){}
+
+    public void getJRecruiterSettingTest(){}
+
+    public void getJRecruiterSettingsTest(){}
+
+    public void getUsersJobsTest(){}
+
+    public void getUsersJobsForStatisticsTest(){}
+
+    public void getUsersJobsForStatistics2Test(){}
+
+    public void saveJRecruiterSettingTest(){}
+
+    public void searchByKeywordTest(){}
+
+    public void sendJobPostingToMailingListTest(){}
+
+    public void updateJobTest(){}
+}
