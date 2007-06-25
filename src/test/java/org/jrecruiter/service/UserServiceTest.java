632510da974915710b8ba6b69f974276022c965b
Gunnar Hillert <gunnar@hillert.com>
2007-06-25 02:14:47 +0000

    Added more tests

---
+++ src/test/java/org/jrecruiter/service/UserServiceTest.java
@@ -0,0 +1,47 @@
+/*
+*	http://www.jrecruiter.org
+*
+*	Disclaimer of Warranty.
+*
+*	Unless required by applicable law or agreed to in writing, Licensor provides
+*	the Work (and each Contributor provides its Contributions) on an "AS IS" BASIS,
+*	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied,
+*	including, without limitation, any warranties or conditions of TITLE,
+*	NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE. You are
+*	solely responsible for determining the appropriateness of using or
+*	redistributing the Work and assume any risks associated with Your exercise of
+*	permissions under this License.
+*
+*/
+package org.jrecruiter.service;
+
+import org.jrecruiter.test.BaseTest;
+
+/**
+ *
+ * @author Gunnar Hillert
+ * @version $Id: JobService.java 93 2007-01-22 02:42:14Z ghillert $
+ */
+public class UserServiceTest extends BaseTest {
+	
+	UserService userService;
+	
+	/**
+	 * @param userService the userService to set
+	 */
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
+	public void addUserTest(){};
+
+	public void getUserTest(){}
+
+	public void updateUserTest(){}
+
+	public void getAllUsersTest(){}
+
+	public void deleteUserTest(){}
+
+	public void sendPasswordTest(){}
+}
