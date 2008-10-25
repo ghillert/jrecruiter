23a05b6173176ce3f329fd52089631c80fbe37fd
ghillert <ghillert@476599a2-9835-11de-a05d-3bd73e269465>
2008-10-25 21:35:32 +0000

    Added dao methods for retrieving paginated user data

--- src/main/java/org/jrecruiter/dao/jpa/UserDaoJpa.java
+++ src/main/java/org/jrecruiter/dao/jpa/UserDaoJpa.java
@@ -16,12 +16,18 @@
 package org.jrecruiter.dao.jpa;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Expression;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.jrecruiter.common.CollectionUtils;
 import org.jrecruiter.dao.UserDao;
 import org.jrecruiter.model.User;
 import org.springframework.stereotype.Repository;
@@ -104,5 +110,83 @@ implements UserDao {
 
             this.remove(user.getId());
         }
+    }
+
+    /**
+     * Method for returning list of available job postings.
+     * @param pageSize Max number of results returned
+     * @param pageNumber Which page are you one?
+     * @param fieldSorted Which field shall be sorted
+     * @param sortOrder What is the sort order?
+     * @return List of jobs.
+     */
+    @SuppressWarnings("unchecked")
+    public List < User > getUsers(final Integer pageSize,
+                                final Integer pageNumber,
+                                Map<String, String> sortOrders,
+                                Map<String, String> userFilters) {
+        List < User > users;
+
+        if (pageSize == null) {
+            throw new IllegalStateException("pageSize must not be null.");
+        }
+        if (pageNumber == null) {
+            throw new IllegalStateException("pageNumber must not be null.");
+        }
+
+        if (sortOrders == null) {
+            sortOrders = CollectionUtils.getHashMap();
+        }
+
+        if (sortOrders.isEmpty()) {
+            sortOrders.put("lastName", "ASC");
+        }
+
+        if (userFilters == null) {
+            userFilters = CollectionUtils.getHashMap();
+        }
+
+
+        Session session = (Session)entityManager.getDelegate();
+        final Criteria criteria = session.createCriteria(User.class);
+
+        for (Entry<String, String> entry : sortOrders.entrySet()) {
+            if (entry.getValue().equalsIgnoreCase("DESC")) {
+                criteria.addOrder(Order.desc(entry.getKey()));
+            } else if (entry.getValue().equalsIgnoreCase("ASC")) {
+                criteria.addOrder(Order.asc(entry.getKey()));
+            } else {
+                throw new IllegalStateException("SortOrder " + entry.getValue() + " is not supported.");
+            }
+        }
+
+        for (Entry<String, String> entry : userFilters.entrySet()) {
+                criteria.add(Restrictions.ilike(entry.getKey(), entry.getValue()));
+        }
+
+        criteria.setFirstResult((pageNumber - 1) * pageSize);
+        criteria.setMaxResults(pageSize);
+
+        users = criteria.list();
+
+        return users;
+    }
+
+
+    /**
+     * Returns the number of totally available jobs in the system.
+     *
+     * @return Total number of jobs
+     * @see org.jrecruiter.dao.JobsDao#getJobsCount()
+     */
+    public Long getUsersCount() {
+
+        Long numberOfUsers = null;
+
+        Session session = (Session)entityManager.getDelegate();
+        org.hibernate.Query query = session.createQuery("select count(*) from User");
+        numberOfUsers = (Long) query.uniqueResult();
+
+        return numberOfUsers;
     }
 }
