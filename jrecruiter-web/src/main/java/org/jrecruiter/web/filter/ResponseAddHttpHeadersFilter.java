59f4ebc25ae78e9d8bffb2d5211ccffb7cdfc8b0
Gunnar Hillert <gunnar@hillert.com>
2009-07-23 03:23:49 +0000

    Several bugfixes

--- jrecruiter-web/src/main/java/org/jrecruiter/web/filter/ResponseAddHttpHeadersFilter.java
+++ jrecruiter-web/src/main/java/org/jrecruiter/web/filter/ResponseAddHttpHeadersFilter.java
@@ -11,7 +11,6 @@ import java.util.TimeZone;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
