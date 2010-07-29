25c5dd7022546bf1f460c6a070b06699a899cfae
Gunnar Hillert <gunnar@hillert.com>
2010-07-29 03:47:03 +0000

    Converted one of the Model classes to Scala

--- jrecruiter-server/src/main/scala/org/jrecruiter/scala/Region.scala
+++ jrecruiter-server/src/main/scala/org/jrecruiter/scala/Region.scala
@@ -1,6 +1,9 @@
 package org.jrecruiter.scala
 
-import javax.persistence.CascadeType;
+import javax.persistence.CascadeType
+import javax.xml.bind.annotation.{XmlElementRef, XmlTransient}
+import org.jrecruiter.model.Job
+import collection.mutable.HashSet;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -9,7 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import javax.persistence.Transient;
-import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlID;
 
 import org.hibernate.annotations.BatchSize;
 import org.hibernate.search.annotations.Analyzer;
@@ -20,9 +23,12 @@ import org.hibernate.search.annotations.Index;
 import org.hibernate.search.annotations.Indexed;
 import org.hibernate.search.annotations.Store;
 
-import scala.reflect.BeanProperty
-
+import scala.reflect.BeanProperty;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import org.jrecruiter.common.CollectionUtils;
+import javax.xml.bind.annotation.XmlValue;
 
 /**
  * Created by IntelliJ IDEA.
@@ -36,34 +42,34 @@ import org.jrecruiter.common.CollectionUtils;
 @BatchSize(size=15)
 @Indexed
 @Analyzer(impl = classOf[org.apache.lucene.analysis.standard.StandardAnalyzer])
+@XmlAccessorType(XmlAccessType.FIELD)
 class Region (
-    var myid : java.lang.Long,
-    var myname : String) {
+    myid : java.lang.Long,
+    myname : String) {
 
-  
+
     def this() = this(null, null)
-
-
-    id = myid
-    name= myname
 
     /** Primary id of the industry */
     @Id @GeneratedValue(generator="hibseq")
     @DocumentId
     @BeanProperty
-    var id : java.lang.Long = null
+    @XmlAttribute
+    var id : java.lang.Long = myid
 
     @Column(name="name", unique=false, nullable=false, insertable=true, updatable=true)
     @Field(index=Index.TOKENIZED, store=Store.YES)
     @BeanProperty
-    var name : String = null
-  
+    @XmlValue
+    @XmlID
+    var name : String = myname
+
 
     @OneToMany(cascade = Array(CascadeType.ALL), fetch=FetchType.LAZY, mappedBy="job")
     @Transient
     @ContainedIn
     @XmlTransient
-    @BeanProperty var jobs = CollectionUtils.getHashSet();
+    var jobs: java.util.Set[Job] = new java.util.HashSet[Job]
 
 
 }
